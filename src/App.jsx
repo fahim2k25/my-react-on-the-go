05ce4afef74fcd122f2fbd0a547aee3b6e9b8030
fahim2k25 <fahim.program@gmail.com>
2025-05-04 15:56:40 +0600

    structed the basic index html

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <section>
+        <img src="../public/nadir.webp" alt="travel buddy" />
+        <h3>Nadir on the Go</h3>
+      </section>
+    </div>
+  )
+}
+
+export default App
