651a9c1029560b6dccdd21fb97cdadd4f2084e21
fahim2k25 <fahim.program@gmail.com>
2025-05-04 16:53:56 +0600

    added Nadir, centered the div and installed tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ function App() {
 
   return (
     <div>
-      <section>
-        <img src="../public/nadir.webp" alt="travel buddy" />
-        <h3>Nadir on the Go</h3>
+      <section className="w-1/3 mx-auto text-center">
+        <img className="rounded-2xl h-56 w-96" src="../public/nadir.webp" alt="travel buddy" />
+        <h3 className="font-bold text-3xl">Nadir on the Go</h3>
       </section>
     </div>
   )
