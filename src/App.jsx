a8bbf42e6cc1f2155b60054ffc948c93ccdd5121
fahim2k25 <fahim.program@gmail.com>
2025-05-05 14:34:57 +0600

    updated the nadir image path suggested by cmd

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div>
       <section className="w-1/3 mx-auto text-center">
-        <img className="rounded-2xl h-56 w-96" src="../public/nadir.webp" alt="travel buddy" />
+        <img className="rounded-2xl h-56 w-96" src="/nadir.webp" alt="travel buddy" />
         <h3 className="font-bold text-3xl">Nadir on the Go</h3>
       </section>
       <Suspense fallback={<p>Yes Mama its loading....</p>}>
