c72eb60d950b8439bbee724eba84b11eec40208a
fahim2k25 <fahim.program@gmail.com>
2025-05-10 02:52:34 +0600

    added an input field and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react'
 import './App.css'
 import Countries from './components/Countries/Countries'
+import Search from './components/Search/Search'
 
 const countryAllPromise = fetch('https://restcountries.com/v3.1/all')
   .then(response => response.json())
@@ -11,6 +12,7 @@ function App() {
       <section className="w-1/3 mx-auto text-center">
         <img className="rounded-2xl h-56 w-96" src="/nadir.webp" alt="travel buddy" />
         <h3 className="font-bold text-3xl">Nadir on the Go</h3>
+        <Search></Search>
       </section>
       <Suspense fallback={<p>Yes Mama its loading....</p>}>
         <Countries countryAllPromise={countryAllPromise}></Countries>
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,15 @@
+
+
+
+const Search = () => {
+    return (
+        <div>
+            <input type="text" placeholder="Search a Country" className="input input-ghost input-lg" />
+            <button className="btn btn-ghost text-gray-400">
+            search
+            </button>
+        </div>
+    );
+};
+
+export default Search;
