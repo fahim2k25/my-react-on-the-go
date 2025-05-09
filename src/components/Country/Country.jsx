9e76240ea7a0bf251a8e10df4e4de6c6dea4f929
fahim2k25 <fahim.program@gmail.com>
2025-05-09 16:40:11 +0600

    Upgrading the cards contiaining Flags

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -13,24 +13,49 @@ const Country = ({ country, handleVisitedCountries }) => {
     const handleVisitClick = () => {
         setVisit(!visit);
         handleVisitedCountries(name["common"]);
-    } 
+    }
 
     return (
-        <div className={`country ${ visit && 'country-visited'}`}>
-            <section>
-                <img className="w-64 h-40" src={flags["png"]} alt={flags["alt"]} />
-            </section>
-            <p>Name: {name["common"] ? name["common"] : "N/A"}</p>
-            <p>Official Name: {name["official"] ? name["official"] : "N/A"}</p>
-            <p>Initial: {altSpellings[0] ? altSpellings[0] : "N/A"}</p>
-            <p>Capital: {country?.capital?.length > 0 ? country?.capital?.[0] : "N/A"}</p>
-            {/* used optional chaining to bypass the error and also used conditional rendering at the same time */}
-            <p>Continent: {continents[0] ? continents[0] : "N/A"}</p>
-            <p>Population: {population.toLocaleString()}</p>
-            <p>Independence: {independent ? "Yes, free" : "No, not free"}</p>
-            <button className={visit ? 'btn btn-accent' : 'btn btn-error'} onClick={handleVisitClick}>{
-                visit ? "Visited" : "Not Visited"
-                }</button>
+        // <div className={`country ${ visit && 'country-visited'}`}>
+        //     <section>
+        //         <img className="w-64 h-40" src={flags["png"]} alt={flags["alt"]} />
+        //     </section>
+        //     <p>Name: {name["common"] ? name["common"] : "N/A"}</p>
+        //     <p>Official Name: {name["official"] ? name["official"] : "N/A"}</p>
+        //     <p>Initial: {altSpellings[0] ? altSpellings[0] : "N/A"}</p>
+        //     <p>Capital: {country?.capital?.length > 0 ? country?.capital?.[0] : "N/A"}</p>
+        //     {/* used optional chaining to bypass the error and also used conditional rendering at the same time */}
+        //     <p>Continent: {continents[0] ? continents[0] : "N/A"}</p>
+        //     <p>Population: {population.toLocaleString()}</p>
+        //     <p>Independence: {independent ? "Yes, free" : "No, not free"}</p>
+        //     <button className={visit ? 'btn btn-accent' : 'btn btn-error'} onClick={handleVisitClick}>{
+        //         visit ? "Visited" : "Not Visited"
+        //         }</button>
+        // </div>
+
+        <div className={`card bg-base-100 w-96 shadow-sm ${visit && 'country-visited'}`}>
+            <figure>
+                <img
+                    src={flags["png"]}
+                    alt={flags["alt"]} />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">{name["common"] ? name["common"] : "N/A"}</h2>
+                <p>Official Name: {name["official"] ? name["official"] : "N/A"}</p>
+                <p>Initial: {altSpellings[0] ? altSpellings[0] : "N/A"}</p>
+                <p>Capital: {country?.capital?.length > 0 ? country?.capital?.[0] : "N/A"}</p>
+                {/* used optional chaining to bypass the error and also used conditional rendering at the same time */}
+                <p>Continent: {continents[0] ? continents[0] : "N/A"}</p>
+                <p>Population: {population.toLocaleString()}</p>
+                <p>Independence: {independent ? "Yes, free" : "No, not free"}</p>
+                <div className={visit ? 'card-actions btn btn-accent' : 'card-actions btn btn-error'} onClick={handleVisitClick}>
+                    <button className="btn btn-primary">
+                        {
+                            visit ? "Visited" : "Not Visited"
+                        }
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
